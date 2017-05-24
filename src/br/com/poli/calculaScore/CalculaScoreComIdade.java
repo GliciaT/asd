cf240e0e1b0751a611d176107df19fadee784d14
Glicia Tigre <gtrc@ecomp.poli.br>
2017-05-24 16:00:18 -0300

    Adicionado interface

---
+++ src/br/com/poli/calculaScore/CalculaScoreComIdade.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.poli.calculaScore;
+
+import br.com.poli.interfaces.CalculaScore;
+import br.com.poli.principal.Partida;
+
+public class CalculaScoreComIdade implements CalculaScore{
+
+    @Override
+    public void calcula(Partida partida) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
