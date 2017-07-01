b645a9d41839a230b6ba307ec68a9b7a0c2a44a2
Dylan Santos <dylangabrielmedeiros@gmail.com>
2017-07-01 16:19:49 -0300

    Frames organizados

--- src/br/com/poli/view/FimDeJogoFrame.java
+++ src/br/com/poli/view/FimDeJogoFrame.java
@@ -111,8 +111,8 @@ public class FimDeJogoFrame extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        JogarNovamente = new javax.swing.JButton();
+        Sair = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -775,17 +775,17 @@ public class FimDeJogoFrame extends javax.swing.JFrame {
         jLabel4.setFont(new java.awt.Font("DejaVu Sans Light", 2, 36)); // NOI18N
         jLabel4.setText("xxxxxxx");
 
-        jButton1.setText("JOGAR NOVAMENTE");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        JogarNovamente.setText("JOGAR NOVAMENTE");
+        JogarNovamente.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                JogarNovamenteActionPerformed(evt);
             }
         });
 
-        jButton2.setText("SAIR");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        Sair.setText("SAIR");
+        Sair.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                SairActionPerformed(evt);
             }
         });
 
@@ -950,7 +950,7 @@ public class FimDeJogoFrame extends javax.swing.JFrame {
                         .addComponent(jTextField11, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jButton1)
+                            .addComponent(JogarNovamente)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jTextField146, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -980,7 +980,7 @@ public class FimDeJogoFrame extends javax.swing.JFrame {
                 .addGap(70, 70, 70))
             .addGroup(layout.createSequentialGroup()
                 .addGap(415, 415, 415)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(Sair, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(183, Short.MAX_VALUE)
@@ -1102,8 +1102,8 @@ public class FimDeJogoFrame extends javax.swing.JFrame {
                     .addComponent(jTextField154, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(68, 68, 68)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(JogarNovamente)
+                    .addComponent(Sair))
                 .addGap(45, 45, 45))
         );
 
@@ -1435,13 +1435,13 @@ public class FimDeJogoFrame extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField8ActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void JogarNovamenteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JogarNovamenteActionPerformed
+        
+    }//GEN-LAST:event_JogarNovamenteActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void SairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SairActionPerformed
         dispose();
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_SairActionPerformed
 
     /**
      * @param args the command line arguments
@@ -1479,8 +1479,8 @@ public class FimDeJogoFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton JogarNovamente;
+    private javax.swing.JButton Sair;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
